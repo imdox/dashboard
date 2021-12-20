caf72a8fc6bcfaa8420150bc2491e628631ea586
Shrawan Shinde <syntaxerror1972@users.noreply.github.com>
2021-12-20 15:09:46 +0530

    db added

---
+++ newDB.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Dec 20, 2021 at 09:39 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `Company`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`, `date`) VALUES
+(4, 'admin@example.com', '$2y$12$knvnvT4sUw80J9LtVCD7VO5r0MJZXQ10..SDF5PMA0cwd31Y5qIkS', '2018-08-07 01:23:11'),
+(5, 'admin@gmail.com', '$2y$12$PfwF/GlUHcDS0QwQY/gNVueBUiUYb4cepXyLw6238nxJYSys5xF6e', '2021-12-11 19:16:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` int(11) NOT NULL,
+  `names` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `names`, `email`, `message`, `date`) VALUES
+(4, 'James Mlamba', 'jaymo@gmail.com', 'I am interested in a meeting.', '2018-07-28 01:38:22'),
+(5, 'James Mlamba', 'ethredah@gmail.com', 'hi', '2018-07-31 19:45:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `author` varchar(200) NOT NULL,
+  `title` varchar(400) NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `author`, `title`, `content`, `date`) VALUES
+(5, 'Ethredah', 'MENTAL HEALTH IS REAL', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,csem.Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. enean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,\r\n\r\n', '2018-07-27 15:28:31'),
+(15, 'hhkjkjjkjkj', 'hjkhjkhjkjkjkj', 'opopoopopopoo ', '2021-12-13 23:54:32'),
+(16, 'sdfsdfsdfs', 'sdfsdfsd', ' sdfsdfsdf', '2021-12-13 23:55:17'),
+(17, 'sdfsdfsdf', 'sdfsdfsdfs', 'sdfdsfsdfs ', '2021-12-13 23:55:24'),
+(18, 'wewewewe', 'qwqwqwqwqw', 'asasasasas ', '2021-12-13 23:55:35'),
+(19, 'ytytytytyty', 'jkjkkjjkjkjjk', ';l;ll;looioioioiiiio ', '2021-12-13 23:55:47'),
+(20, 'sfdfsdfsf', 'dsfdsfsdf', 'sdfsdfsdfsd ', '2021-12-14 00:49:04'),
+(21, 'dodge', 'dgdggg', 'gffggfgfg ', '2021-12-14 18:27:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `f_name` varchar(100) DEFAULT NULL,
+  `l_name` varchar(100) DEFAULT NULL,
+  `password` varchar(1000) DEFAULT NULL,
+  `mobile` int(10) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `rpc_code` varchar(100) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `f_name`, `l_name`, `password`, `mobile`, `city`, `country`, `rpc_code`, `date`) VALUES
+(3, 'ethredah@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-07-27 18:21:30'),
+(4, 'james@hack3.io', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-07-27 18:21:30'),
+(6, 'admin@pikash.sales', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2018-07-28 01:49:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `image` varchar(1000) DEFAULT NULL,
+  `video` varchar(1000) NOT NULL,
+  `blogid` int(11) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `videos`
+--
+
+INSERT INTO `videos` (`id`, `name`, `description`, `image`, `video`, `blogid`, `date`) VALUES
+(1, 'Jaden', NULL, NULL, 'Awesome post guys!!', 6, '2018-07-28 00:15:59'),
+(6, 'Cliff', NULL, NULL, 'I really relate to this', 5, '2018-07-28 01:00:14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `blogid` (`blogid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
